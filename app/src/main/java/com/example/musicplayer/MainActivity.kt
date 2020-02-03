45ae0a5781fb3c61049b53a04d450cca58cba409
Venue 11 Pro <robin839168gmail.com>
2020-02-03 12:24:04 +0200

    start

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.jerry as jerry1
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +14,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun tomJERRY(view: View) {
+        val tom = tom
+        val jerry = jerry
         tom.animate().alpha(0F).duration=2000
-        jerry1.animate().alpha(1F).duration=2000
-        if (this.jerry1) {
+        jerry.animate().alpha(1F).duration = 2000
+        if (jerry.alpha==1F) {
             tom.animate().alpha(1F).duration=2000
-            jerry1.animate().alpha(0F).duration=2000
+            jerry.animate().alpha(0F).duration=2000
 
         }
 
