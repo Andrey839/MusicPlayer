60e162630e705450e11d59cfee325903a19c4e7d
Venue 11 Pro <robin839168gmail.com>
2020-02-06 12:35:03 +0200

    ui Music Player

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,28 +1,30 @@
 package com.example.musicplayer
 
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.MediaController
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.max
+import kotlin.properties.Delegates
 
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun tomJERRY(view: View) {
-        val tom = tom
-        val jerry = jerry
-        tom.animate().alpha(0F).duration=2000
-        jerry.animate().alpha(1F).duration = 2000
-        if (jerry.alpha==1F) {
-            tom.animate().alpha(1F).duration=2000
-            jerry.animate().alpha(0F).duration=2000
-
-        }
 
     }
+
+
 }
+
