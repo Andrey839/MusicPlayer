856d3a67182eeee625232afed89957d3a2e66fa2
Venue 11 Pro <robin839168gmail.com>
2020-02-09 09:55:39 +0200

    notPlaying Music Player

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.musicplayer
 
 import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.SeekBar
@@ -13,46 +14,48 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var player: MediaPlayer
     lateinit var zeek : SeekBar
-    val play = playImageView
-    val listSong = listOf(R.raw.beau, R.raw.zonderling, R.raw.disclosure)
-    var counter = 0
+    private val listSong = listOf(R.raw.beau, R.raw.zonderling, R.raw.disclosure)
+    private var counter = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val player = MediaPlayer.create(this,listSong[counter])
+        val player = MediaPlayer.create(this, listSong[counter])
         zeek = seekBar
         zeek.max = player.duration
+        songsTextView?.text = listSong[counter].toString()
+        val uri = Uri.parse("android.resource://"+packageName+"/"+listSong[counter])
+
 
         zeek?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean){
                 if (fromUser) player.seekTo(progress)
-                if (progress == zeek.max) {
-                    player.start()
-                    play.setImageResource(R.drawable.ic_play_arrow_orang_24dp)
-                    if (counter==2) counter=0
-                    else counter++
-                songsTextView.text = listSong[counter].toString()}
-            }
+         }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
             }
         })
 
         Timer().scheduleAtFixedRate(object : TimerTask(){
             override fun run() {
-                zeek.progress = (player.currentPosition)
+                zeek.progress = player.currentPosition
             }
         },0,1000)
 
-
+        player.setOnCompletionListener {
+            if (counter==2) counter = 0
+            else counter++
+            player.reset()
+            player?.setDataSource(this,uri)
+            player.prepare()
+            player.start()
+        }
 
     }
 
@@ -61,24 +64,30 @@ class MainActivity : AppCompatActivity() {
         if (counter==0) counter=2
         else counter--
         player.start()
-        songsTextView.text = listSong[counter].toString()
+        songsTextView?.text = listSong[counter].toString()
     }
     fun next(view: View) {
         player.stop()
         if (counter==2) counter=0
         else counter++
         player.start()
-        songsTextView.text = listSong[counter].toString()
+        songsTextView?.text = listSong[counter].toString()
     }
-    fun play(view: View) {
-        if (player.isPlaying) {
-            player.pause()
-            play.setImageResource(R.drawable.ic_pause_orang_24dp)
-        } else {player.start()
-        play.setImageResource(R.drawable.ic_play_arrow_orang_24dp)
-            if (counter==2) counter=0
-            else counter++}
-        songsTextView.text = listSong[counter].toString()
+
+
+    fun play( view: View) {
+            //      if (player.isPlaying) {
+            //          player.pause()
+            //          playImageView?.setImageResource(R.drawable.ic_pause_orang_24dp)
+            //      } else {
+            player.prepare()
+            player.start()
+
+            //          playImageView?.setImageResource(R.drawable.ic_play_arrow_orang_24dp)
+            //          if (counter==2) counter=0
+            //          else counter++}
+            songsTextView?.text = listSong[counter].toString()
+
 
     }
 
